2d24a21abf23236e34273a1c915e208c30047dc3
buudee625 <liu.andrew625@gmail.com>
2022-10-01 21:04:43 -0700

    styling

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Menu, Header, Image, Dropdown } from 'semantic-ui-react';
+import './PageHeader.css';
 
 export default function PageHeader({ loggedUser, handleLogout }) {
   console.log(loggedUser, '<-- loggedUser in header');
   return (
-    <Menu inverted>
+    <Menu borderless>
       <Menu.Item>
         <Header>
           <Link to="/">KoyoMe</Link>
@@ -26,7 +27,7 @@ export default function PageHeader({ loggedUser, handleLogout }) {
         </Menu.Item>
         <Dropdown item icon="bars">
           <Dropdown.Menu>
-            <Dropdown.Item>
+            <Dropdown.Item link>
               <Link to="/events" style={{ color: 'black' }}>
                 All Events
               </Link>
